35be3c3532efbc477984334822b1691227fd4dfd
Ian Eve Perry <ian.eve.perry@gmail.com>
2021-01-05 12:55:25 -0800

    add bot routine

--- bot.py
+++ bot.py
@@ -1,7 +1,21 @@
 import tweepy
 import logging
 import config
+import lot
 
 twitter_api = config.create_twitter_api()
 street_view_key = config.get_street_view_api_key()
 
+def main():
+    nl = lot.NiceLot()
+
+    nl.get_image(street_view_key)
+
+    nl.upload_image(twitter_api)
+
+    nl.post_tweet(twitter_api)
+
+    nl.mark_as_tweeted()
+
+if __name__ == '__main__':
+    main()
